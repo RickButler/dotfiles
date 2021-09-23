3e9bbfff07eb7e8c30445249da7bc6811c83b37a
Rick Butler <rick@rickbutler.com>
2021-09-23 11:23:46 -0400

    shared powershell profile

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,8 @@
 oh-my-posh --init --shell pwsh --config "~/rickbutler.omp.json" | Invoke-Expression
+
+if($IsWindows) {
+    Import-Module posh-git
+    Import-Module posh-sshell
+    Import-Module "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
+    Enter-VsDevShell d9b5e285
+}
