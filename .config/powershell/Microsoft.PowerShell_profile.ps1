720cc41dc5a397ea1f02f34dc552454e34c0f678
Rick Butler <rick@rickbutler.com>
2022-03-15 23:21:09 -0400

    change vswhere path validation

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,20 +1,25 @@
 function GetVSInfo {
-    return & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -Latest -format json | ConvertFrom-Json
+    $path = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+    if(Test-Path $path) {
+        return & $path -Latest -format json | ConvertFrom-Json
+    } 
+    return $false;    
 }
 
 oh-my-posh --init --shell pwsh --config "~/profile.omp.json" | Invoke-Expression
 Import-Module Terminal-Icons
 
-
 if($IsWindows) {
     $VSInfo = GetVSInfo
-    $VSInstallPath = $VSInfo | select-object -ExpandPropert installationPath
-    $VSInstanceId = $VSInfo | select-object -ExpandPropert instanceId
+    if($VSInfo){        
+        $VSInstallPath = $VSInfo | select-object -ExpandPropert installationPath
+        $VSInstanceId = $VSInfo | select-object -ExpandPropert instanceId
 
-    if($VSInstanceID -and $VSInstallPath) { 
-        $DevShellPath = Get-ChildItem -Recurse -Path $VSInstallPath -Include Microsoft.VisualStudio.DevShell.dll | Select-Object -First 1
-        Import-Module $DevShellPath
-        Enter-VsDevShell $VSInstanceId
+        if($VSInstanceID -and $VSInstallPath) { 
+            $DevShellPath = Get-ChildItem -Recurse -Path $VSInstallPath -Include Microsoft.VisualStudio.DevShell.dll | Select-Object -First 1
+            Import-Module $DevShellPath
+            Enter-VsDevShell $VSInstanceId
+        }
     }
 
     Import-Module posh-sshell   
